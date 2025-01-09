7e1ee3e839f935f4380279a111b08a878c8946b7
elza-2001 <elzik998@gmail.com>
2025-01-09 14:56:30 +0500

    домашнее задание 4
    Создала возможность добавлять в список элементы, при нажатии на которые, они зачеркиваются

--- script.js
+++ script.js
@@ -7,3 +7,30 @@
 /* Если кликнуть повторно уже на зачеркнутый, он снова становится обычным */
 
 /* Очищать input после добавления нового элемента в список */
+
+const sendInput = document.querySelector('#input');
+const itemsContainer = document.querySelector('#items');
+
+sendInput.addEventListener('keydown', function(event) {
+    const inputText = sendInput.value;
+
+    const newItem = document.createElement('div');
+    newItem.classList.add('items');
+
+    if (event.key == 'Enter') {
+        newItem.textContent = inputText;
+        input.value = '';
+    };
+
+    if (inputText != '') {
+        itemsContainer.append(newItem);
+    }
+
+    newItem.addEventListener('click', function (){
+        newItem.classList.toggle('done');
+    });
+});
+
+
+
+
